5089a7a72634fb9797257c5fdff2659265efcd69
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-17 17:58:57 +0900

    feat:ヘッダーの実装

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,29 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>トップページ</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.17/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+  <!-- ヘッダーの表示 -->
+  <header class="bg-blue-500">
+    <nav class="mt-2 mr-2">
+      <ul class="flex justify-end space-x-4">
+        <li>
+          <a class="text-white hover:text-blue-800" href="/">HOME</a>
+        </li>
+        <li>
+          <a class="text-white hover:text-blue-800" href="#">収入TOP</a>
+        </li>
+        <li>
+          <a class="text-white hover:text-blue-800" href="#">支出TOP</a>
+        </li>
+        <li>
+          <a class="text-white hover:text-blue-800" href="#">ログイン</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
