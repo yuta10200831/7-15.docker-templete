2900e6a9777369a9e379d20757a44a8134ab118f
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-17 18:11:01 +0900

    fix:ヘッダー微調整

--- src/public/index.php
+++ src/public/index.php
@@ -5,25 +5,27 @@
   <title>トップページ</title>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.17/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-  <!-- ヘッダーの表示 -->
-  <header class="bg-blue-500">
-    <nav class="mt-2 mr-2">
-      <ul class="flex justify-end space-x-4">
-        <li>
-          <a class="text-white hover:text-blue-800" href="/">HOME</a>
-        </li>
-        <li>
-          <a class="text-white hover:text-blue-800" href="#">収入TOP</a>
-        </li>
-        <li>
-          <a class="text-white hover:text-blue-800" href="#">支出TOP</a>
-        </li>
-        <li>
-          <a class="text-white hover:text-blue-800" href="#">ログイン</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+<body class="bg-gray-100">
+  <div class="mx-auto my-8" style="width: 80%;">
+    <header class="bg-blue-500">
+      <nav>
+        <ul class="flex justify-between py-2 px-4">
+          <li class="mr-10">
+            <a class="text-white hover:text-blue-800" href="/">HOME</a>
+          </li>
+          <li class="mr-10">
+            <a class="text-white hover:text-blue-800" href="#">収入TOP</a>
+          </li>
+          <li class="mr-10">
+            <a class="text-white hover:text-blue-800" href="#">支出TOP</a>
+          </li>
+          <li>
+            <a class="text-white hover:text-blue-800" href="#">ログイン</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  </div>
 </body>
 </html>
