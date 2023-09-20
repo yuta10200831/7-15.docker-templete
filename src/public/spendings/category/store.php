cf887e6c26965f7a8140c691851122722629aaee
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-20 21:04:08 +0900

    add:バリデーション追加

--- src/public/spendings/category/store.php
+++ src/public/spendings/category/store.php
@@ -10,14 +10,25 @@ if (empty($categories)) {
     $error_messages[] = "カテゴリ名が入力されていません";
 }
 
+// DB接続
+$pdo = new PDO('mysql:host=mysql;dbname=kakeibo;charset=utf8', 'root', 'password');
+
+// 既に存在するカテゴリかどうかをチェック
+$checkStmt = $pdo->prepare("SELECT * FROM categories WHERE name = :name");
+$checkStmt->bindParam(':name', $categories, PDO::PARAM_STR);
+$checkStmt->execute();
+
+// カテゴリが既に存在する場合はエラーメッセージを設定
+if ($checkStmt->fetch()) {
+    $error_messages[] = "既に登録済みのカテゴリです";
+}
+
+// エラーメッセージが存在する場合はセッションに保存してリダイレクト
 if (!empty($error_messages)) {
     $_SESSION['error'] = implode(', ', $error_messages);
     header('Location: create.php');
     exit;
 }
-
-// DB接続
-$pdo = new PDO('mysql:host=mysql;dbname=kakeibo;charset=utf8', 'root', 'password');
 
 // SQL文でデータを挿入
 $stmt = $pdo->prepare("INSERT INTO categories (user_id, name) VALUES (0, :name)");
