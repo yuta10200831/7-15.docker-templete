d4accd04d3655eabf20f737adbb526c33f849821
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-02-21 00:15:37 +0900

    interfaceの修正

--- src/app/Domain/Port/IIndexQuery.php
+++ src/app/Domain/Port/IIndexQuery.php
@@ -5,4 +5,5 @@ namespace App\Domain\Port;
 interface IIndexQuery {
     public function getMonthlySummary($year): array;
     public function getSpendingsWithFilter($year, $categoryId = null, $startDate = null, $endDate = null): array;
+    public function getIncomesWithFilter($year, $categoryId = null, $startDate = null, $endDate = null): array; // 追加
 }
